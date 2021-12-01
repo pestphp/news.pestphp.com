db2ceb91bddcbd0b516455d05abd25c1c8a0c213
Luke Downing <lukeraymonddowning@gmail.com>
2021-12-01 20:36:04 +0000

    Fixes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,7 +64,7 @@ final class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequestsWithRedis::class,
+        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
 }
