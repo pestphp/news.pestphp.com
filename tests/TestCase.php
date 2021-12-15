fd9b2b44bcd23d91e847052d673b58161273c35a
Luke Downing <lukeraymonddowning@gmail.com>
2021-12-15 11:58:30 +0000

    WIP

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,7 +6,6 @@ namespace Tests;
 
 use Database\Seeders\EmailListSeeder;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
